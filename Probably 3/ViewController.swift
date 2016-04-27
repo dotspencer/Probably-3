dbd78d5ee5b29dad7f83f4a1163428b421adec95
Spencer Smith <dontaskspencer@gmail.com>
2016-04-26 23:51:19 -0600

    Added colors

--- Probably 3/ViewController.swift
+++ Probably 3/ViewController.swift
@@ -17,11 +17,24 @@ class ViewController: UIViewController {
     var colors = [
         UIColor.clearColor(),
         UIColor.clearColor(),
-        UIColor(red:0.00, green:0.69, blue:0.93, alpha:1.00),
-        UIColor(red:0.80, green:0.36, blue:0.36, alpha:1.00),
-        UIColor(red:1.00, green:0.84, blue:0.00, alpha:1.00),
-        UIColor(red:0.11, green:0.91, blue:0.26, alpha:1.00),
-        UIColor(red:0.76, green:0.16, blue:0.91, alpha:1.00)
+        UIColor(red:0.11, green:0.69, blue:0.93, alpha:1.00), // 3
+        UIColor(red:0.80, green:0.36, blue:0.36, alpha:1.00), // 4
+        UIColor(red:1.00, green:0.84, blue:0.19, alpha:1.00), // 5
+        UIColor(red:0.38, green:0.87, blue:0.44, alpha:1.00), // 6
+        UIColor(red:0.86, green:0.42, blue:0.97, alpha:1.00), // 7
+        UIColor(red:0.99, green:0.63, blue:0.16, alpha:1.00), // 8
+        UIColor(red:0.63, green:0.76, blue:0.88, alpha:1.00), // 9
+        UIColor(red:0.94, green:0.99, blue:0.21, alpha:1.00), // 10
+        UIColor(red:0.99, green:0.25, blue:0.73, alpha:1.00), // 11
+        UIColor(red:0.92, green:0.84, blue:0.63, alpha:1.00), // 12
+        UIColor(red:0.39, green:0.65, blue:0.62, alpha:1.00), // 13
+        UIColor(red:0.95, green:0.51, blue:0.42, alpha:1.00), // 14
+        UIColor(red:0.05, green:0.43, blue:0.55, alpha:1.00), // 15
+        UIColor(red:0.89, green:0.46, blue:0.12, alpha:1.00), // 16
+        UIColor(red:0.71, green:0.82, blue:0.32, alpha:1.00), // 17
+        UIColor(red:0.65, green:0.24, blue:0.36, alpha:1.00), // 18
+        UIColor(red:0.65, green:0.77, blue:0.73, alpha:1.00), // 19
+        UIColor(red:0.42, green:0.49, blue:0.98, alpha:1.00) // 20
     ]
     
     var score: Int = 0
@@ -55,6 +68,9 @@ class ViewController: UIViewController {
         
         buttons[0].tag = 0
         buttons[1].tag = 1
+        
+        scoreLabel.layer.masksToBounds = true
+        scoreLabel.layer.cornerRadius = 2
     }
     
     @IBAction func click(sender: UIButton) {
